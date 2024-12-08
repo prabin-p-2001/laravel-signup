592c13da550089962d4df5647160a1f8d6c35ab2
prabin-p-2001 <prabinprnv@gmail.com>
2024-12-08 10:30:07 +0530

    change design

--- userAuth/resources/views/welcome.blade.php
+++ userAuth/resources/views/welcome.blade.php
@@ -1,49 +1,42 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('./css/style.css')}}">
+    <title>Document</title>
 </head>
-
 <body>
-    <h1>SIGNUP!</h1>
+<div class="blob"></div>
 
-    <section class="form-section">
-        <div class="container ">
-            <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">First Name</label>
-                    <input type="text" class="form-control" name="firstname">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Last Name</label>
-                    <input type="text" class="form-control" name="lastname">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Mobile Number</label>
-                    <input type="tel" class="form-control" name="mobile_number">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" name="password">
-                </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary mb-5">Submit</button>
-            </form>
-        </div>
-    </section>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<div class="wraper">
+  <form>
+    <h2>Login</h2>
+    <div class="input-box">
+      <input type="email" required>
+      <label>Email</lable>
+    </div>
+
+    <div class="input-box">
+      <input type="password" required>
+      <label>Password</lable>
+    </div>
+
+    <div class="remember-forgot">
+      <label><input type="checkbox">
+        remember me </label>
+      <a href="#">forgot password?</a>
+    </div>
+
+    <button>Login</button>
+
+    <div class="register-link">
+      <p>don't have an account?
+        <a href="#">register</a>
+      </p>
+    </div>
+
+  </form>
+</div>
 </body>
-
 </html>
